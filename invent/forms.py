43a5b11b84ac28ae79f39a39b919deb5a6a439df
tkrasuski <tkrasuski@gazeta.pl>
2018-01-10 15:25:25 +0100

    Minor forms changes

--- invent/forms.py
+++ invent/forms.py
@@ -3,8 +3,8 @@ from django import forms
 class NameForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
 class InventoryPart(forms.Form):
-    part_no = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Part no'}))
-    description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Description'}))
-    notes = forms.CharField(required=False,widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Notes'}))
+    part_no = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-sm','placeholder':'Part no'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control form-control-sm','placeholder':'Description'}))
+    notes = forms.CharField(required=False,widget=forms.Textarea(attrs={'class':'form-control form-control-sm','placeholder':'Notes'}))
     #unit = forms.ForeignKey('Units')
    # active = forms.BooleanField(required=False)
