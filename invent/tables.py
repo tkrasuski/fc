51f839b2649a4c7573819d2148cdcd81e3b97563
tkrasuski <tkrasuski@gazeta.pl>
2018-01-16 12:36:18 +0100

    django_tables2 support

--- invent/tables.py
+++ invent/tables.py
@@ -1,7 +1,13 @@
 import django_tables2 as tables
 from .models import InventoryParts
+from django.utils.safestring import mark_safe, SafeText
 
 class InventoryPartsTable(tables.Table):
+    def render_part_no(self,value, record):
+        url = record.id
+        return mark_safe('<a href="%s">%s</a>' % (url, record))
+
     class Meta:
         model = InventoryParts
         template = 'django_tables2/bootstrap.html'
+        exclude = ('id', 'note')
